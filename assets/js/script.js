13f8546879f7ee40f880aa2f0d1ed430a448fcd0
Foad <fwad40901@gmail.com>
2023-10-14 18:49:14 +0330

    Add Section Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-/* created by zmat-cli */
+const toggleBtn = document.querySelector('.bi-list-nested');
+const closeBtn = document.querySelector('.bi-x');
+const navAside = document.querySelector('.navbarAside');
+const showNavside = function () {
+  navAside.classList.add('show-navAside');
+};
+const hideNavside = function () {
+  navAside.classList.remove('show-navAside');
+};
+toggleBtn.addEventListener('click', showNavside);
+closeBtn.addEventListener('click', hideNavside);
