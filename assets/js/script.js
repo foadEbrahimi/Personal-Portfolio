89fa55eed6eadbd4a777d00e4e0bdf8f39f187b1
Foad <fwad40901@gmail.com>
2023-10-14 19:37:09 +0330

    Add Section Hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 const toggleBtn = document.querySelector('.bi-list-nested');
 const closeBtn = document.querySelector('.bi-x');
 const navAside = document.querySelector('.navbarAside');
+// Create Function for NavbarAside Controls
 const showNavside = function () {
   navAside.classList.add('show-navAside');
 };
 const hideNavside = function () {
   navAside.classList.remove('show-navAside');
 };
+// Actice Functions
 toggleBtn.addEventListener('click', showNavside);
 closeBtn.addEventListener('click', hideNavside);
