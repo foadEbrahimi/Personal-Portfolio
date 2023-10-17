e415a93d037be822adf41d41e9b76ac8f651aa27
Foad <fwad40901@gmail.com>
2023-10-17 17:19:50 +0330

    Fix Bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 const toggleBtn = document.querySelector('.bi-list-nested');
 const closeBtn = document.querySelector('.bi-x');
 const navAside = document.querySelector('.navbarAside');
+const Navlinks = document.querySelectorAll('.navbar-link');
+
+for (let i = 0; i < Navlinks.length; i++) {
+  Navlinks[i].addEventListener('click', function () {
+    navAside.classList.remove('show-navAside');
+    Navlinks.classList.add('active-links-primary');
+  });
+}
+
 // Create Function for NavbarAside Controls
 const showNavside = function () {
   navAside.classList.add('show-navAside');
